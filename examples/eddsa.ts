9b3966d360777526982c5053b272a6cc4391c2e6
Florian <florian@technotro.com>
2025-03-19 15:04:26 +0700

    add examples

---
+++ examples/eddsa.ts
@@ -0,0 +1,30 @@
+import { ZkProgram, Bool, Bytes } from 'o1js';
+import { createEddsa, createForeignTwisted, TwistedCurves } from 'o1js-eddsa';
+
+export { eddsa, Edwards25519, Eddsa, Bytes32 };
+
+class Edwards25519 extends createForeignTwisted(TwistedCurves.Edwards25519) {}
+class Scalar extends Edwards25519.Scalar {}
+class Eddsa extends createEddsa(Edwards25519) {}
+class Bytes32 extends Bytes(32) {}
+
+const eddsa = ZkProgram({
+  name: 'eddsa',
+  publicInput: Bytes32,
+  publicOutput: Bool,
+
+  methods: {
+    verifyEddsa: {
+      privateInputs: [Eddsa, Edwards25519],
+      async method(
+        message: Bytes32,
+        signature: Eddsa,
+        publicKey: Edwards25519
+      ) {
+        return {
+          publicOutput: signature.verify(message, publicKey),
+        };
+      },
+    },
+  },
+});
